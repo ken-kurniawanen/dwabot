93a5be24d1ade068fcae89040b053441b6b793bd
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2017-02-13 12:59:24 +0700

    fix require

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,3 +1,18 @@
 <?php
-phpinfo();
+/* Database Configuration */
+$hostname   = "localhost";
+$database   = "kotor"; 
+$username   = "kotor";    
+$password   = "jokem123";   
+
+try {
+
+    $dbo = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+    print_r($dbo);
+} catch (PDOException $e) {
+
+    $msg = $e->getMessage();
+    echo $msg;
+    die();
+}
 ?>
