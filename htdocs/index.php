a957736f5740fc1c8ccf24cbc113c7f644db373a
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2017-02-13 12:52:21 +0700

    fix require

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,6 +12,7 @@ $password   = "jokem123";
 try {
 
     $dbo = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+    print_r($dbo);
 } catch (PDOException $e) {
 
     $msg = $e->getMessage();
